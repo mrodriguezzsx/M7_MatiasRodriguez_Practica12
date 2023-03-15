84c8c5365e310f1553a91cf62e4bcdbec543559e
mrodriguezzsx <mrodrigu@jaumebalmes.net>
2023-03-15 18:54:09 +0100

    Practica 13

--- m7_matias_practica12/routes/admin.php
+++ m7_matias_practica12/routes/admin.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\ControladorAdmin;
 
 Route::group(['middleware'=>['adb']],function(){
-    Route::get('articulos', [ControladorAdmin::class, 'articulos']);
-    Route::get('clientes', [ControladorAdmin::class, 'clientes']);
-    Route::get('facturacion', [ControladorAdmin::class, 'facturacion']);
-    Route::get('contabilidad', [ControladorAdmin::class, 'contabilidad']);
+    Route::get('articulos', [ControladorAdmin::class, 'articulos'])->name('articulos');
+    Route::get('clientes', [ControladorAdmin::class, 'clientes'])->name('clientes');
+    Route::get('facturacion', [ControladorAdmin::class, 'facturacion'])->name('facturacion');
+    Route::get('contabilidad', [ControladorAdmin::class, 'contabilidad'])->name('contabilidad');
 });
